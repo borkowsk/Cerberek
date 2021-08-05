6f9822a65182735dfceafdd15f0e196a0481ba89
Wojciech T. Borkowski <wborkowsk@gmail.com>
2021-08-05 15:30:13 +0200

    Szukacz and same doc link added

---
+++ tools/szukaczC/szukacz.c
@@ -0,0 +1,73 @@
+#define UNICODE 1
+#define _UNICODE 1
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <psapi.h>
+
+// To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
+// and compile with -DPSAPI_VERSION=1
+
+void PrintModules( DWORD processID )
+{
+    HMODULE hMods[1024];
+    HANDLE hProcess;
+    DWORD cbNeeded;
+    unsigned int i;
+
+    // Print the process identifier.
+
+    printf( "\nProcess ID: %u\n", processID );
+
+    // Get a list of all the modules in this process.
+
+    hProcess = OpenProcess( PROCESS_QUERY_INFORMATION |
+                            PROCESS_VM_READ,
+                            FALSE, processID );
+    if (NULL == hProcess)
+        return;
+
+    if( EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded))
+    {
+        for ( i = 0; i < (cbNeeded / sizeof(HMODULE)); i++ )
+        {
+            TCHAR szModName[MAX_PATH];
+
+            // Get the full path to the module's file.
+
+            if ( GetModuleFileNameEx( hProcess, hMods[i], szModName,
+                                      sizeof(szModName)/sizeof(TCHAR)))
+            {
+                // Print the module name and handle value.
+
+                _tprintf( TEXT("\t%s (0x%08X)\n"), szModName, hMods[i] );
+            }
+        }
+    }
+
+    CloseHandle( hProcess );
+}
+
+void main( )
+{
+    // Get the list of process identifiers.
+
+    DWORD aProcesses[1024], cbNeeded, cProcesses;
+    unsigned int i;
+
+    if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+        return;
+
+    // Calculate how many process identifiers were returned.
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    // Print the name of the modules for each process.
+
+    for ( i = 0; i < cProcesses; i++ )
+        PrintModules( aProcesses[i] );
+}
+
+//ZOBACZ TE¯:
+//http://msdn.microsoft.com/en-us/library/ms682621%28v=vs.85%29.aspx
